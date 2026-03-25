e8ca1a88352f3c2ee3b54bb89e9a74d79186e48a
Vineethk08 <milatariv@gmail.com>
2026-03-25 03:36:17 -0400

    update

--- data.tf
+++ data.tf
@@ -23,7 +23,7 @@ data "aws_ssm_parameter" "private_subnet_ids"{
 }
 
 data "aws_ssm_parameter" "sg_id"{
-  name = "/${var.project}/${var.environment}/${var.component}/sg_id"
+  name = "/${var.project}/${var.environment}/${var.component}_sg_id"
 }
 data "aws_ssm_parameter" "vpc_id"{
   name = "/${var.project}/${var.environment}/${var.component}/vpc_id"
